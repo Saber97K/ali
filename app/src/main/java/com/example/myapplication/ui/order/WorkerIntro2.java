8446b32e5b626769b574bf299046f97cc50ae011
Yap-Yoong-Siew <s2122194@siswa.um.edu.my>
2023-01-13 15:49:52 +0800

    final
    finalllll

--- app/src/main/java/com/example/myapplication/ui/order/WorkerIntro2.java
+++ app/src/main/java/com/example/myapplication/ui/order/WorkerIntro2.java
@@ -2,25 +2,23 @@ package com.example.myapplication.ui.order;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import com.example.myapplication.GeneralModule.MainLobby;
-import com.example.myapplication.MainActivity3;
 import com.example.myapplication.R;
 import com.example.myapplication.Session;
 import com.example.myapplication.added_wallet.MainActivity3_wallet;
 import com.example.myapplication.databinding.WorkerIntro2Binding;
-import com.example.myapplication.ui.Utils.CategoryAdapter;
-import com.example.myapplication.ui.Utils.CategoryManage;
 import com.example.myapplication.ui.Utils.OrdersManage;
 import com.example.myapplication.ui.Utils.UsersManage;
 import com.example.myapplication.ui.Utils.database.SQLiteManager;
@@ -30,36 +28,50 @@ public class WorkerIntro2 extends Fragment {
     private WorkerIntro2Binding binding;
     private View root;
 
-    private TextView typeTExtView, DateTextView, priceTextView , descTextView;
-    private int currentUser , OrderId;
+    private TextView typeTExtView, DateTextView, priceTextView , descTextView,phoneTextView;
+    private int currentUser , OrderId,anotherUser;
     private OrdersManage ordersManage;
     private SQLiteManager sqLiteManager;
-    private UsersManage usersManage;
+    private UsersManage usersManage,usersManage1;
+    private ImageView imageView;
+
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = WorkerIntro2Binding.inflate(inflater, container, false);
         root = binding.getRoot();
+        currentUser = ((Session) getActivity().getApplication()).getSomeVariable();
+        initWidgets();
+        UsersManage.UsersList.clear();
+        OrdersManage.orderArrayList.clear();
         sqLiteManager = SQLiteManager.instanceOfDatabase(getActivity());
         sqLiteManager.populateOrderListArray();
-
-        currentUser = ((Session) getActivity().getApplication()).getSomeVariable();
-
+        sqLiteManager.populateUserListArray();
 
         for (int i =0 ; i < OrdersManage.orderArrayList.size(); i++){
             if(OrdersManage.orderArrayList.get(i).getAccepted_user_id() == currentUser){
                if(OrdersManage.orderArrayList.get(i).getStatus() == 0){
                    ordersManage  = OrdersManage.orderArrayList.get(i);
+
                }
             }
         }
         for (int i = 0 ; i < UsersManage.UsersList.size(); i++){
             if(UsersManage.UsersList.get(i).getId() == currentUser){
                 usersManage = UsersManage.UsersList.get(i);
-                break;
+
             }
         }
+        for (int i = 0 ; i < UsersManage.UsersList.size(); i++){
+            if(UsersManage.UsersList.get(i).getId() == anotherUser){
+                usersManage1 = UsersManage.UsersList.get(i);
+
+            }
+        }
+
+
+
 
         Button cancel = (Button) root.findViewById(R.id.button50);
         cancel.setOnClickListener(new View.OnClickListener() {
@@ -80,10 +92,12 @@ public class WorkerIntro2 extends Fragment {
 
     private void initWidgets() {
 
-        typeTExtView = getActivity().findViewById(R.id.textView35);
-        priceTextView = getActivity().findViewById(R.id.textView36);
-        DateTextView = getActivity().findViewById(R.id.textView37);
+        typeTExtView = getActivity().findViewById(R.id.textView15);
+        priceTextView = getActivity().findViewById(R.id.textView37);
+        DateTextView = getActivity().findViewById(R.id.textView36);
         descTextView = getActivity().findViewById(R.id.textView38);
+        phoneTextView = getActivity().findViewById(R.id.textViewPhoneNumber1);
+        imageView = getActivity().findViewById(R.id.imageView412);
 
 
     }
